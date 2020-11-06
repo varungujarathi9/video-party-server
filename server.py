a5e9ef072576fcbe61db9e3de5d24fb1b433ed94
gcp-server <varungujarathi9@video-party.us-central1-a.c.tonal-vector-294213.internal>
2020-11-06 04:12:36 +0000

    bug fixes

--- server.py
+++ server.py
@@ -125,16 +125,16 @@ def handler():
                         read_fileSize = int(data['read_filesize'])
                         
                         progress = tqdm.tqdm(range(read_fileSize), f"Receiving {read_filename}", unit="B", unit_scale=True, unit_divisor=1024)
-                        with open(filename, "wb") as f:
+                        with open(read_filename, "wb") as f:
                             for _ in progress:
-                            # read 1024 bytes from the socket (receive)
-                            bytes_read = json.loads(client_socket.recv(4096))
-                            if not bytes_read:    
-                                return 
+                                # read 1024 bytes from the socket (receive)
+                                bytes_read = json.loads(client_socket.recv(4096))
+                                if not bytes_read:    
+                                    return 
                             
-                            f.write(bytes_read)
-                            # update the progress bar
-                            progress.update(len(bytes_read))
+                                f.write(bytes_read)
+                                # update the progress bar
+                                progress.update(len(bytes_read))
 
                 if(sendDataFlag):
                     send_to_all_clients(room_sockets[room_id].values(), json.dumps({'join':room_id, 'room':room_details[room_id]}))
