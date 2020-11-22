075db1238bde3f4837eebb62ea5af342c491f363
gcp-server <v.gujarathi777@gmail.com>
2020-11-22 12:39:22 +0000

    mid leave

--- server_rest.py
+++ server_rest.py
@@ -1,9 +1,12 @@
 
-from flask import Flask,request,redirect,current_app
-from flask_socketio import SocketIO,emit,join_room,leave_room
+from flask import Flask,request,redirect,current_app, jsonify
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import json
 import string
 import random
+import datetime
+import pytz
+import json
 
 #instantiate 
 app = Flask(__name__)
@@ -12,19 +15,22 @@ app.config['SECRET_KEY'] = 'videoparty100'
 #wrapping flask instance with the socketio wrapper
 socketIo = SocketIO(app,cors_allowed_origins="*")
 
-ROOM_ID_ARRAY=[]
+timezone = pytz.timezone('Asia/Kolkata')
+
+rooms_details = {}
  
-def get_room_id():
+def get_room_id(length):
     letters_and_digits = string.ascii_uppercase + string.digits
-    result_str = ''.join((random.choice(letters_and_digits) for i in range(6)))
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
     print("Random alphanumeric String is:", result_str)
     return result_str
 
-
-@socketIo.on('connect')
-def client_connect():
-    emit("connected",{'msg':"welcome to videoparty"})
-    print("client connectecd")
+@socketIo.on('create-room')
+def create_room(data):
+    room_id = get_room_id(6)
+    rooms_details[room_id] = {'members':[data['username']],'created_at':datetime.datetime.now(tz=timezone).strftime('%x @ %X'),  'video_name': None, 'paused':True, 'playing_at':0, 'total_duration': 0}
+    join_room(room_id)
+    emit('room-created', {'room-id':room_id, 'room-details':rooms_details[room_id]})
 
 @socketIo.on('message')
 def handleMessage(data):   
