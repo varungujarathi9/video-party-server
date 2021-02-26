5384d24e42c37b48ee2ee7f49406bcacfec03f0a
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-02-26 23:07:03 +0530

    fix 1

--- server_rest.py
+++ server_rest.py
@@ -148,7 +148,7 @@ if __name__ == '__main__':
     #automatic reloads again when made some changes
     app.debug=True
     # use this while running in gcp server
-    socketIo.run(app, host='0.0.0.0', port=os.environ['PORT'])
+    socketIo.run(app, host='0.0.0.0', port=5000)
 # from flask import Flask
 # app = Flask(__name__)
 
